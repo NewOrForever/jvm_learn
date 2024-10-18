c11bcf7de11cf5c744c9f3fcba71603452c2d322
sq <shenqian@yuhang100.com>
2024-10-18 09:29:28 +0800

    jvm之class文件字节码学习

---
+++ learn04_class_byte_code_parse/src/main/java/org/example/TulingByteCode.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * ClassName:TulingByteCode
+ * Package:org.example
+ * Description:
+ *
+ * @Date:2024/10/16 10:52
+ * @Author:qs@1.com
+ */
+public class TulingByteCode {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+}
